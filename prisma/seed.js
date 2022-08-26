6c299c9521c807e106b41fa540ecb9c7600daaf4
Michele M <mnmin@gmail.com>
2022-08-26 15:06:55 +0100

    Movie and Screening Prisma and Seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,16 +4,44 @@ const prisma = new PrismaClient();
 async function seed() {
     const createdCustomer = await prisma.customer.create({
         data: {
-            name: 'Alice'
-        }
+            name: 'Alice',
+            contact: {
+                create: {
+                    phone: "123456789",
+                    email: "alice@worderland.com"
+                }
+            }
+        },
+            include: {
+            contact: true,
+            }            
     });
 
     console.log('Customer created', createdCustomer);
 
     // Add your code here
+    // const createdContact = await prisma.contact.create({
+    //     data: {
+    //         phone: "123456789",
+    //         email: "alice@wonderland.com"
+    //     }
+    // })
 
+    //console.log('Contact created', createdContact)
 
+    const createdMovie = await prisma.movie.create({
+        data: {
+            title: "The Matrix",
+            runtimeMins: 126
+        }
+    });
+    console.log("Movie created", createdMovie)
 
+    const createdScreening = await prisma.screening.create({
+        data: {
+            startsAt: new Date('December 17, 1995 03:24:00')
+        }
+    })
 
     // Don't edit any of the code below this line
     process.exit(0);
